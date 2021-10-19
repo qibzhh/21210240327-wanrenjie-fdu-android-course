8f5a4976dbfc4702693f7fde528846cb0cb17aba
wanrenjie <93505228@qq.com>
2021-10-19 21:09:54 +0800

    chapter 1

---
+++ FDU-Android course-Chapter 1/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,25 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    public Button button;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        button=findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                button.setText("Welcome!");
+                Log.d("你好，欢迎！", "21210240327 万仁杰");
+            }
+        });
+    }
+}
